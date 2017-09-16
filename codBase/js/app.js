c16fc35a53a5184230393ec49b9c47be62c50c78
Miguel Saueda <migsauceda@gmail.com>
2017-09-16 12:24:17 -0600

    ya esta lista la manera como manejar el evento clic

--- codBase/js/app.js
+++ codBase/js/app.js
@@ -1,8 +1,19 @@
+/* 1547
+Saber cuando se hace click en cada una de las teclas
+Identificar la tecla para luego saber que hacer
+*/
+
+//identifica las teclas
 var Tecla= document.getElementsByClassName('tecla')
 
-for(var i= 0; i < Tecla.length; i++){
-    Tecla[i].addEventListener('click', OperarTecla())
+//hace lo correspondiene con la tecla que recibio el evento click
+function OperarTecla(pEvento){
+  var evento= pEvento;
+
+  alert(evento.currentTarget.id)
 }
-function OperarTecla(){
-  alert("hacer tecla as grande ")
+
+//asigna el evento clic a cada tecla identificada en la variable Tecla[]
+for(var i= 0; i < Tecla.length; i++){
+  document.getElementById(Tecla[i].id).onclick= OperarTecla;
 }
