d6c7bb8a848e3ec87addf56ba21df87ea280d47c
Miguel Saueda <migsauceda@gmail.com>
2017-09-18 14:51:07 -0600

    modelo de estados 3: v1

--- codBase/js/app.js
+++ codBase/js/app.js
@@ -1,10 +1,11 @@
 //variables globales para operandos, operadores, Resultado
-var Operando1, Operando2
+var tmp
 var Operador
 var Resultado
 var Estado
 var TipoLeido
 var Leido
+var Operaciones= new Array()
 
 //inicializa
 Operando2= 0
@@ -51,18 +52,28 @@ function EvaluarEstados(Leido){
     switch (Leido){
       case "dividido":
         Operador= "/"
+        TipoLeido= "operador"
         break;
       case "por":
         Operador= "*"
+        TipoLeido= "operador"
         break;
       case "menos":
         Operador= "-"
+        TipoLeido= "operador"
         break;
       case "mas":
         Operador= "+"
+        TipoLeido= "operador"
+        break;
+      case "sign":
+        TipoLeido= "sign"
+        break
+      case "igual":
+        TipoLeido= "igual"
         break;
       }
-      TipoLeido= "operador"
+
   }
 
   //control de estados
@@ -70,52 +81,68 @@ function EvaluarEstados(Leido){
     case "eEspera": //0
       if (TipoLeido== "digito"){
         Estado= "eOperando1"
-        Operando1= Leido
-        document.getElementById("display").innerHTML = Operando1
+        //Operando1= Leido
+        Operaciones[0]= Leido //el primer operando
+        document.getElementById("display").innerHTML = Operaciones[0]
       }
       break;
 
     case "eOperando1": //1
       if (TipoLeido== "digito"){
-        Operando1+= Leido
-        document.getElementById("display").innerHTML = Operando1
+        //Operando1+= Leido
+        Operaciones[0]+= Leido //el primer operando
+        document.getElementById("display").innerHTML = Operaciones[0]
       }
       else if (TipoLeido== "operador"){
         Estado= "eOperador"
+        Operaciones[1]= Operador
+      }
+      if (TipoLeido== "sign"){
+        tmp= Operaciones[0]
+        tmp= Number(tmp) * -1
+        Operaciones[0]= String(tmp) //el primer operando
+        document.getElementById("display").innerHTML = Operaciones[0]
       }
       break;
 
     case "eOperador": //2
       if (TipoLeido== "digito"){
         Estado= "eOperando2"
-        Operando2= Leido
-        document.getElementById("display").innerHTML = Operando2
+        //Operando2= Leido
+        Operaciones[2]= Leido //el segundo operando
+        document.getElementById("display").innerHTML = Operaciones[2]
+      }
+      else if (TipoLeido== "igual") {
+        RealizarCalculo()
       }
       break;
 
     case "eOperando2": //3
       if (TipoLeido== "digito"){
-        Operando2+= Leido
-        document.getElementById("display").innerHTML = Operando2
+        //Operando2+= Leido
+        Operaciones[2]+= Leido //el segundo operando
+        document.getElementById("display").innerHTML = Operaciones[2]
       }
-      else if (TipoLeido== "Operador"){
+      else if (TipoLeido== "operador"){
         Estado= "eOperador"
+        Operaciones[3]= Operador
         RealizarCalculo()
+        document.getElementById("display").innerHTML = Operaciones[0]
       }
-      else  if (Leido== "igual"){
+      else if (Leido== "sign") {
+        Number(Operaciones[2])*= -1 //el primer operando
+        document.getElementById("display").innerHTML = Operaciones[2]
+      }
+      else  if (TipoLeido== "igual"){
         Estado= "eIgual"
         RealizarCalculo()
       }
       break;
 
     case "eIgual":  //4
-    alert(Leido)
       if (TipoLeido== "operador"){
         Estado= "eOperador"
-        RealizarCalculo()
-      }
-      else if (Leido== "igual") {
-        RealizarCalculo()
+        Operaciones[1]= Operador
       }
       break;
 
@@ -132,26 +159,26 @@ function EvaluarEstados(Leido){
 function RealizarCalculo(){
   switch (Operador){
     case "/":
-      if (Number(Operando2) <= 0){
+      if (Number(Operaciones[2]) <= 0){
         Resultado= "Error"
       }
       else{
-        Resultado= Number(Operando1) / Number(Operando2)
+        Resultado= Number(Operaciones[0]) / Number(Operaciones[2])
       }
       break;
 
     case "*":
-      Resultado= Number(Operando1) * Number(Operando2)
+      Resultado= Number(Operaciones[0]) * Number(Operaciones[2])
       break;
 
     case "-":
-      Resultado= Number(Operando1) - Number(Operando2)
+      Resultado= Number(Operaciones[0]) - Number(Operaciones[2])
       break;
 
     case "+":
-      Resultado= Number(Operando1) + Number(Operando2)
+      Resultado= Number(Operaciones[0]) + Number(Operaciones[2])
       break;
     }
     document.getElementById("display").innerHTML = Resultado
-    Operando1= Resultado
+    Operaciones[0]= Resultado
 }
